2c1d5bcea7d30e594b010ce4a651baf68a02f0df
zakiay <zakiay.su@gmail.com>
2014-06-09 17:52:35 -0400

    removed timer

--- p004.py
+++ p004.py
@@ -1,10 +1,4 @@
-import timeit # I use this to see the runtime. Best one so far was 0.72
-
-start = timeit.default_timer()
-
 def isPal(n):
-	"""This function checks to see if the number is a palindrom.
-	Must input a string like this: isPal(str(12345))"""
 	strNumber = str(n)
 
 	if len(n) % 2 == 0: # length is even, must compare first with last, and so on	
@@ -25,7 +19,3 @@ for i in range(100, 1000):
 				temp = i*j
 
 print "\nPalindrom: \t", temp
-
-stop = timeit.default_timer()
-
-print "Runtime: \t", stop - start, "\n"
